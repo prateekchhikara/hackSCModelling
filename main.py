7ce890574daa329b34d285ae8719053b7a141427
Prateek Chhikara <prateekchhikara24@gmail.com>
2023-02-04 17:31:52 -0800

    gcp commit

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
 from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
+import requests
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
 
 class Model:
     
@@ -16,12 +21,12 @@ class Model:
         self.class_names = open("labels.txt", "r").readlines()
     
     
-    def predict(self, file_path):
+    def predict(self, image):
         
         data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
         # Replace this with the path to your image
-        image = Image.open(file_path).convert("RGB")
+        # image = Image.open(file_path).convert("RGB")
 
         # resizing the image to be at least 224x224 and then cropping from the center
         size = (224, 224)
@@ -41,19 +46,32 @@ class Model:
         index = np.argmax(prediction)
         class_name = self.class_names[index]
         confidence_score = prediction[0][index]
-
-        # Print prediction and confidence score
-        # print("Class:", class_name[2:], end="")
-        # print("Confidence Score:", confidence_score)
         return class_name[2:], confidence_score
         
-        
 
-if __name__ == "__main__":
+# @app.route("/")
+# def fun():
+#     return "Omkar"
+
+@app.route("/", methods = ["POST"])
+def hello_world():
+    id = request.files.get('file')
+    
+    print(id)
+    print(type(id))
+    img = Image.open(request.files['file']).convert("RGB")
+    
+    class_name, confidence_score = obj.predict(img)
+    
+    return jsonify({
+        "tag" : class_name[:-1]
+    })
+
+
+
+if __name__ == '__main__':
     obj = Model()
-    class_name, confidence_score = obj.predict("240.jpg")
-    print(class_name, confidence_score)
-        
+    app.run(debug=True, port=8080, host="127.0.0.1")
 
 
 
